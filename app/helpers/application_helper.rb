980edb3a4955ffe07d8cdadd74cab9ca80483094
Christopher Bertels <bakkdoor@flasht.de>
2008-10-09 00:39:34 +0200

    menu ueberarbeitet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def menu_items
-    menu_items = {
-                  "Home" => :home, "Kunden" => :customers, "Projekte" => :projects,
-                  "Aufgaben" => :tasks, "Arbeitszeiten" => :worktimes
-                 }
+    menu_items = []
+    menu_items << {:title => "Home", :controller => :home}
+    menu_items << {:title => "Kunden", :controller => :customers}
+    menu_items << {:title => "Projekte", :controller => :projects}
+    menu_items << {:title => "Aufgaben", :controller => :tasks}
+    menu_items << {:title => "Arbeitszeiten", :controller => :worktimes}
+    
   end
 end
