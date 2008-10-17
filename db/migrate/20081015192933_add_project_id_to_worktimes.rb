77c044611a89ac1aed9d3efb1a06a4ce252caf4e
Christopher Bertels <bakkdoor@flasht.de>
2008-10-17 14:57:33 +0200

    entfernt

--- db/migrate/20081015192933_add_project_id_to_worktimes.rb
+++
@@ -1,9 +0,0 @@
-class AddProjectIdToWorktimes < ActiveRecord::Migration
-  def self.up
-    add_column :worktimes, :project_id, :integer, :null => false
-  end
-
-  def self.down
-    remove_column :worktimes, :project_id
-  end
-end
