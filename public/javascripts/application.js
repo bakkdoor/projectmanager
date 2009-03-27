fc5c6e7a591051acbfb935fe5dc711691d0fb556
Christopher Bertels <bakkdoor@flasht.de>
2009-03-28 00:29:36 +0100

    fixed bug, that displayed wrong worktime list at initial loading of worktimes view

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,4 @@
-function load_worktimes() {
+function show_correct_worktime() {
     var project_id = $('project_selection').value;
     $$('div.worktimes_project_listing').invoke('hide');
     Effect.Appear("worktimes_project_" + project_id);
