2a4114bf3d64ef930bc76f679cffc86fd2a97d5b
Christopher Bertels <bakkdoor@flasht.de>
2009-03-29 17:20:31 +0200

    changed menu links to use locale text and fixed a typo in home#index view

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,50 +1,50 @@
 module MenuHelper
-  
+
   def menu_items
     menu_items = []
     menu_items << {:title => "Home", :icon => "house", :url => root_path}
     if logged_in?
       if current_user.admin?
-        menu_items << { :title => "Kunden",
-                        :icon => "user_suit", 
-                        :url => customers_path, 
+        menu_items << { :title => t('words.customers'),
+                        :icon => "user_suit",
+                        :url => customers_path,
                         :controller => :customers }
-                        
-        menu_items << { :title => "Projekte", 
-                        :icon => "layout_content", 
-                        :url => projects_path, 
+
+        menu_items << { :title => t('words.projects'),
+                        :icon => "layout_content",
+                        :url => projects_path,
                         :controller => :projects }
-                        
-        menu_items << { :title => "Aufgaben", 
-                        :icon => "table", 
-                        :url => all_tasks_path, 
+
+        menu_items << { :title => t('words.tasks'),
+                        :icon => "table",
+                        :url => all_tasks_path,
                         :controller => :tasks }
-                        
-        menu_items << { :title => "Arbeitszeiten", 
-                        :icon => "time", 
-                        :url => all_worktimes_path, 
+
+        menu_items << { :title => t('words.worktimes'),
+                        :icon => "time",
+                        :url => all_worktimes_path,
                         :controller => :worktimes }
       end
-      menu_items << { :title => "Mitarbeiter", 
-                      :icon => "group", 
-                      :url => users_path, 
+      menu_items << { :title => t('words.employees'),
+                      :icon => "group",
+                      :url => users_path,
                       :controller => :users }
     end
-    
+
     menu_items
   end
-  
+
   def menu_links
     @output = []
     menu_items.each do |item|
       link_style =  params[:controller].to_s == item[:controller].to_s ? "active_link" : ""
-      
+
       title = item[:title]
       if(item[:icon])
         title = icon_tag(item[:icon])
         title += " #{item[:title]}"
       end
-      
+
       @output << (link_to title, item[:url], {:class => link_style})
       #@output << " | "
     end
